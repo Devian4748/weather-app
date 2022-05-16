c8414c70e034feee38a7adcc8eb9157dae90a0e8
Devian4748 <devian4748@gmail.com>
2022-05-17 02:29:54 +0900

    Add Current Weather Info

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,41 +1,171 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import weatherCodes from './weather-code';
+import GlobalStyle from './GlobalStyle';
+
 const BASE_WEATHER_URL = 'http://api.weatherstack.com';
 const WEATHER_KEY = '86ca8648b874c1a2add4d0e181daff0b';
-const FETCH_CURRENT_WEATHER = `${BASE_WEATHER_URL}/current?access_key=${WEATHER_KEY}&query=Seoul`;
+
+const Logo = styled.h1`
+  font-size: 3rem;
+  text-transform: uppercase;
+  padding: 1rem 2rem;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+const CurrentWeather = styled.article`
+  width: 85vw;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem;
+`;
+
+const WeatherInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const WeatherSummaryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const WeatherTemperature = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const WeatherDetailInfoBox = styled.div`
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+`;
+
+const WeatherDetailInfoItem = styled.p`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 0;
+  gap: 1.5rem;
+`;
+
+const Loader = styled.p`
+  font-size: 2rem;
+  text-align: center;
+`;
 
 const App = () => {
+  const [currentWeather, setCurrentWeather] = useState(() => {
+    return JSON.parse(localStorage.getItem('current_weather') ?? '{}');
+  });
+
+  // 2022.05.16 김명진
+  // localStorage 에 현재 날짜가 저장되어 있지 않다면 weather api 를 통해 데이터를 불러오고
+  // localStorage 에 저장한다.
+  useEffect(() => {
+    // 갱신 조건
+    let isNeedRefresh = false;
+
+    const isEmpty = Object.keys(currentWeather).length === 0;
+    if (isEmpty) {
+      // localStorage 에 저장된 데이터가 없거나
+      isNeedRefresh = true;
+    } else {
+      // 저장된 날짜가 오늘 날짜가 아닌 경우,
+      const date = new Date();
+      const currentDate = date.getDate();
+
+      const localDateInfo = currentWeather.location.localtime
+        .split(' ')[0]
+        .split('-');
+      const savedDate = Number.parseInt(
+        localDateInfo[localDateInfo.length - 1],
+        10
+      );
+
+      if (currentDate !== savedDate) {
+        isNeedRefresh = true;
+      }
+    }
+
+    if (isNeedRefresh) {
+      fetch(`${BASE_WEATHER_URL}/current?access_key=${WEATHER_KEY}&query=Seoul`)
+        .then(response => response.json())
+        .then(data => {
+          setCurrentWeather(data);
+          localStorage.setItem('current_weather', JSON.stringify(data));
+        });
+    }
+  }, [currentWeather]);
+
+  // 2022.05.16 김명진
+  // current_weather 로부터 현재 날씨의 정보를 읽어온다.
+  let findWeatherCode = null;
+  if (currentWeather.current) {
+    findWeatherCode = weatherCodes.find(weatherCode => {
+      return weatherCode.overhead_code === currentWeather.current.weather_code;
+    });
+  }
+
   return (
     <>
-      <h1>Devi Weather</h1>
+      <GlobalStyle />
+      <Logo>Devi Weather Cast</Logo>
       <main>
-        {/** 현재 날씨 */}
-        <div>
-          {/** 현재 날씨 - 요약 */}
-          <div>
-            {/** 날씨 이미지 : weather-current-weather_icons */}
-            <img
-              src='https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'
-              alt='current-weather'
-            />
-            {/** 날씨 상태 : weather-current-weather_code */}
-            <p>113</p>
-          </div>
+        <CurrentWeather>
+          {currentWeather.location && (
+            <h2>
+              <span>{currentWeather.location.country}</span>
+              <span>&#44;</span>
+              <span>{currentWeather.location.name}</span>
+            </h2>
+          )}
+          <WeatherInfo>
+            <WeatherSummaryInfo>
+              <img
+                src={currentWeather.current?.weather_icons?.[0] ?? ''}
+                alt='current-weather-icon'
+              />
+              <p>{findWeatherCode && findWeatherCode.trans_text_day}</p>
+            </WeatherSummaryInfo>
 
-          {/** 현재 날씨 - 온도 */}
-          <div>
-            {/** 현재 날씨 - 측정 온도 */}
-            <p>14 &#8451;</p>
-            {/** 현재 날씨 - 온도 측정 시간대 */}
-            <p>03:25 PM</p>
-          </div>
+            <WeatherTemperature>
+              <p>
+                {currentWeather.current?.temperature ?? 'Loading...'}&#8451;
+              </p>
+            </WeatherTemperature>
 
-          {/** 현재 날씨 - 상세 */}
-          <div>
-            <span>Wind : 4 kmph</span> {/** 현재 날씨 - 상세 - 바람 */}
-            <span>Precip : 0 mm</span> {/** 현재 날씨 - 상세 - 강수량 */}
-            <span>Pressure : 1018 mb</span> {/** 현재 날씨 - 상세 - 기압 */}
-          </div>
-        </div>
-        {/** 1주일간의 날씨들 List */}
+            <WeatherDetailInfoBox>
+              <WeatherDetailInfoItem>
+                <span>바람</span>
+                <span>
+                  {currentWeather.current?.wind_speed ?? 'loading...'} kmph
+                </span>
+              </WeatherDetailInfoItem>
+              <WeatherDetailInfoItem>
+                <span>강수량</span>
+                <span>{currentWeather.current?.precip ?? 'loading...'} mm</span>
+              </WeatherDetailInfoItem>
+              <WeatherDetailInfoItem>
+                <span>기압</span>
+                <span>
+                  {currentWeather.current?.pressure ?? 'loading...'} mb
+                </span>
+              </WeatherDetailInfoItem>
+            </WeatherDetailInfoBox>
+          </WeatherInfo>
+        </CurrentWeather>
       </main>
     </>
   );
---
+++ weather-app/src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    line-height: 1;
+    background-color: #3C4A90;
+    color: #fff;
+  }
+`;
